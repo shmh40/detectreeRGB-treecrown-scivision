3c78664b808064e7b91b289f5f8af3d0e62f4a5d
Alejandro © <acocac@gmail.com>
2021-12-08 14:46:34 +0000

    create class for each model

---
+++ src/model.py
@@ -0,0 +1,54 @@
+import numpy as np
+import urllib.request
+# machine learning libraries
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.utils.visualizer import Visualizer, ColorMode
+from detectron2.config import get_cfg
+from detectron2.engine import DefaultTrainer
+
+
+class DeepForest:
+    def __init__(self):
+        pass
+
+    def predict(self, image: np.ndarray) -> np.ndarray:
+        return image
+
+
+class DetectreeRGB:
+    def __init__(self, model_source: str = "zenodo"):
+        # define the URL to retrieve the model
+        if model_source == 'zenodo':
+            fn = 'model_final.pth'
+            url = f'https://zenodo.org/record/5515408/files/{fn}?download=1'
+            urllib.request.urlretrieve(url, fn)
+
+        cfg = get_cfg()
+
+        # if you want to make predictions using a CPU, run the following line. If using GPU, hash it out.
+        cfg.MODEL.DEVICE = 'cpu'
+
+        # model and hyperparameter selection
+        cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml"))
+        cfg.DATALOADER.NUM_WORKERS = 2
+        cfg.SOLVER.IMS_PER_BATCH = 2
+        cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
+
+        ### path to the saved pre-trained model weights
+        cfg.MODEL.WEIGHTS = fn
+
+        # set confidence threshold at which we predict
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.15
+
+        #### Settings for predictions using detectron config
+        self.pretrained_model = DefaultPredictor(cfg)
+
+    def predict(self, image: np.ndarray) -> np.ndarray:
+
+        y = self.pretrained_model.predictor(image)
+        return y
+
+
+if __name__ == "__main__":
+    pass
